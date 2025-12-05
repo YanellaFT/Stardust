c878ff129095b10228353f8627154eb2a5c4149a
yanellaft <yanellaft@gmail.com>
2025-12-05 17:07:56 -0500

    added sparkle in css and started js

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //color pallete
+    const colors = [
+
+    ]
+
+    //Define an SVG template for a star shape
+    //path uses coordinates to draw a 5-pointed star
+    //fill="currentColor" makes it use the current text color
+    const starSVG = ` <svg viewBox="0 0 24 24">
+                    <path d="M12 1L14.8 8.2L22.5 9.2L16.7 14.5L18.4 22L12 18.2L5.6 22L7.3 14.5L1.5 
+9.2L9.2 8.2L12 1Z" 
+                    fill="currentColor"/>
+                </svg>`;
+
+    //store cursor position
+    const cursorHistory = [];
+    const maxHistory = 15;
+    let lastEmitTime = 0; //milliseconds
+
+    //Listen for mouse movement events
+    document.addEventListener('mousemove', (e) => {
+        //current timestamp in milliseconds
+        const now = Date.now();
+
+        //Add the current cursor data to the history array: stores x/y position, timestamp, and velocity
+        cursorHistory.push ({ //e. is event
+            x: e.clientX, //cursor x position
+            y: e.clientY, //cursor y position
+            time: now, //current timestamp
+            vx: e.movementX, //cursor velocity x
+            vy: e.movementY //cursor velocity y
+        });
+
+        //Remove old positions
+        if (cursorHistory.length > maxHistory) {
+            cursorHistory.shift(); //removes oldest (first) element in array
+        }
+
+        //CREATE SPARKLESSS 
+        const speed = Math.sqrt((e.movementX)^2 + (e.movementY)^2);
+    })
+})
